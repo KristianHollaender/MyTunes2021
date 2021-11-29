42e1046fbd8e6a87f4b8b40a1cc665a8bd60b37e
KristianHollaender <kristianhansen9@hotmail.dk>
2021-11-29 12:07:46 +0100

    Added database connector

--- MyTunes/src/dal/db/DatabaseConnector.java
+++ MyTunes/src/dal/db/DatabaseConnector.java
@@ -1,5 +1,41 @@
 package dal.db;
 
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+
+    private SQLServerDataSource dataSource;
+
+
+    public DatabaseConnector()
+    {
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName("10.176.111.31");
+        dataSource.setDatabaseName("MyTunesGruppe6");
+        dataSource.setUser("CSe21A_5");
+        dataSource.setPassword("CSe21A_5");
+        dataSource.setPortNumber(1433);
+    }
+
+
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+
+        DatabaseConnector databaseConnector = new DatabaseConnector();
+
+        try (Connection connection = databaseConnector.getConnection()) {
+
+            System.out.println("Is it open? " + !connection.isClosed());
+
+        } //Connection gets closed here
+    }
 }
