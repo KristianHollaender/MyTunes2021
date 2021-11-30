9300a7fa3fa48289caccc201aafb18c85ba0a295
KristianHollaender <kristianhansen9@hotmail.dk>
2021-11-30 13:10:41 +0100

    Made getSongs method

--- src/dal/SongsDAO.java
+++ src/dal/SongsDAO.java
@@ -1,14 +1,57 @@
 package dal;
 
+import be.Song;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import dal.db.DatabaseConnector;
 
 import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SongsDAO {
 
-        private DatabaseConnector databaseConnector;
+    private DatabaseConnector databaseConnector;
 
-        public SongsDAO() throws IOException {
-            databaseConnector = new DatabaseConnector(); //New Database object, used to creating the connection.
+    public SongsDAO() throws IOException {
+        databaseConnector = new DatabaseConnector(); //New Database object, used to creating the connection.
+    }
+
+    public List<Song> getSongs() {
+        ArrayList<Song> allSongs = new ArrayList<>();
+
+        try (Connection connection = databaseConnector.getConnection()) {
+            String sqlStatement = "SELECT * FROM Song";
+            Statement statement = connection.createStatement();
+
+            if (statement.execute(sqlStatement)) {
+                ResultSet resultSet = statement.getResultSet();
+
+                while (resultSet.next()) {
+                    int ID = resultSet.getInt("ID");
+                    String title = resultSet.getString("title");
+                    String artist = resultSet.getString("artist");
+                    String category = resultSet.getString("category");
+                    Time duration = resultSet.getTime("duration");
+
+                    Song song = new Song(ID, title, artist, category, duration); // Creating a song object from the retrieved values
+                    allSongs.add(song); // Adding the song to the ArrayList
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            return null;
         }
+        return allSongs;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        SongsDAO songsDAO = new SongsDAO();
+
+        List<Song> allSongs = songsDAO.getSongs();
+
+        System.out.println(allSongs);
+    }
+
 }
