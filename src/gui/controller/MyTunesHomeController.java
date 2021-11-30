f29930c4127b35e2f6b284756d9f7c889426d4cb
Andylaa10 <andylaa100@gmail.com>
2021-11-30 11:02:04 +0100

    FXML MyTunesHomeView

--- src/gui/controller/MyTunesHomeController.java
+++ src/gui/controller/MyTunesHomeController.java
@@ -2,14 +2,52 @@ package gui.controller;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
 public class MyTunesHomeController{
+
+    @FXML
+    private Button btnSongBack;
+    @FXML
+    private Button btnSongPlayer;
+    @FXML
+    private Button btnSongForward;
+    @FXML
+    private Slider sliderSound;
+    @FXML
+    private Button btnSearchbar;
+    @FXML
+    private Button btnNewPlaylist;
+    @FXML
+    private Button btnEditPlaylist;
+    @FXML
+    private Button btnDeletePlaylist;
+    @FXML
+    private Button btnLeftArrow;
+    @FXML
+    private Button btnUp;
+    @FXML
+    private Button btnDown;
+    @FXML
+    private Button btnDeleteSongsOnPlaylist;
+    @FXML
+    private Button btnNewSong;
+    @FXML
+    private Button btnEditSong;
+    @FXML
+    private Button btnDeleteSong;
+    @FXML
+    private Button btnClose; 
+
 
     public void createNewSong(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/gui/view/EditSongs.fxml"));
