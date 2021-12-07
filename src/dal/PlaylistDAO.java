76c31343ad83458557613aad9b402e5971617e68
Andylaa10 <andylaa100@gmail.com>
2021-12-07 09:13:11 +0100

    playlist

--- src/dal/PlaylistDAO.java
+++ src/dal/PlaylistDAO.java
@@ -1,7 +1,6 @@
 package dal;
 
 import be.Playlist;
-import be.Song;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
 import dal.db.DatabaseConnector;
 
@@ -42,6 +41,21 @@ public class PlaylistDAO {
     }
 
     public Playlist createPlaylist(String title) throws SQLServerException {
+        String sql = "INSERT INTO Playlist(Title) values (?,);";
+        Connection connection = databaseConnector.getConnection();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, title);
+            preparedStatement.executeUpdate();
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            int id = 0;
+            if (resultSet.next()) {
+                id = resultSet.getInt(1);
+            }
+            Playlist playlist = new Playlist(id, title);
+            return playlist;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
     }
 
@@ -55,7 +69,8 @@ public class PlaylistDAO {
 
     public static void main(String[] args) throws SQLException {
         PlaylistDAO playlistDAO = new PlaylistDAO();
-        List<Playlist> allPlaylist = playlistDAO.getPlaylist();
+        //List<Playlist> allPlaylist = playlistDAO.getPlaylist();
+        List<Playlist> allPlaylist = (List<Playlist>) playlistDAO.createPlaylist("Nicklas er lidt gay");
         System.out.println(allPlaylist);
     }
 
