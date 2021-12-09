d7621c1c22574138ccddaf62a19adf228e19ceb3
Ferniss <mathiasf2000@hotmail.com>
2021-12-09 17:46:43 +0100

    all works

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,5 @@ public class Main extends Application {
         primaryStage.setTitle("My Tunes 2021");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
-        primaryStage.setResizable(false);
     }
 }
