71a276ab37d87f41a43343ddc94f042eaae45dbf
Ferniss <mathiasf2000@hotmail.com>
2021-12-14 12:15:25 +0100

    Added private

--- src/gui/model/PlaylistModel.java
+++ src/gui/model/PlaylistModel.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class PlaylistModel {
 
-    PlaylistManager playlistManager;
+    private PlaylistManager playlistManager;
 
     /**
      * Constructor for PlaylistModel
